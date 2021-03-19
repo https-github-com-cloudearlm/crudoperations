f1ebd77290cc9ab6c1466ee6b88c9e8bf3c40055
Srilekha Jagarlamudi <srilekhajagarlamudi.ce@gmail.com>
2021-03-19 04:13:51 -0700

    second commit

--- crudfbvproject/urls.py
+++ crudfbvproject/urls.py
@@ -19,5 +19,8 @@ from testapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.show_view), 
+    url(r'^$', views.show_view),
+    url(r'^create/', views.create_view),
+    url(r'^delete/(?P<id>\d+)/$', views.delete_view),
+    url(r'^update/(?P<id>\d+)/$', views.update_view),
 ]
--- testapp/models.py
+++ testapp/models.py
@@ -5,4 +5,5 @@ class Employee(models.Model):
     eno=models.IntegerField()
     ename=models.CharField(max_length=64)
     esal=models.FloatField()
-    eaddr=models.CharField(max_length=256) 
+    eaddr=models.CharField(max_length=256)
+    
